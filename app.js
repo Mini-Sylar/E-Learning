615e06e1b976c37a9d416aa0a85d637da6ab9659
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-02-14 16:35:14 +0000

    :)

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ let ham = document.querySelector(".hamburger");
 let menu = document.querySelector(".mobilemenu");
 let cards  = document.querySelector(".gallery")
 
+
 ham.addEventListener("click", function () {
   menu.classList.toggle("showlinks");
 
   if ($(window).width() <= 425 && menu.classList.contains("showlinks")) {
      cards.style.display = "none";
   } else if($(window).width() <= 425 && !menu.classList.contains("showlinks")){
-     cards.style.display = "block";
+     cards.style.display = "flex";
   }
 
 });
