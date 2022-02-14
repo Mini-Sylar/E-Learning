3ef1e17d5258f37e0a98948824cb9f7ca5c9a44e
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-02-14 16:05:12 +0000

    Desktop Animations & Final Touches

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 let ham = document.querySelector(".hamburger");
 let menu = document.querySelector(".mobilemenu");
+let cards  = document.querySelector(".gallery")
+
 ham.addEventListener("click", function () {
   menu.classList.toggle("showlinks");
+  if (menu.classList.contains("showlinks")){
+      cards.style.display = "none"
+  }
+  else{
+     cards.style.display = "block";
+  }
 });
