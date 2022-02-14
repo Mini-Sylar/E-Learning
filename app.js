8faefb0031ea1a737dc92074179703eec27b6fd0
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-02-14 16:25:05 +0000

    small screefix

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ let cards  = document.querySelector(".gallery")
 
 ham.addEventListener("click", function () {
   menu.classList.toggle("showlinks");
-  if (menu.classList.contains("showlinks")){
-      cards.style.display = "none"
-  }
-  else{
+
+  if ($(window).width() <= 425 && menu.classList.contains("showlinks")) {
+     cards.style.display = "none";
+  } else if($(window).width() <= 425 && !menu.classList.contains("showlinks")){
      cards.style.display = "block";
   }
+
 });
